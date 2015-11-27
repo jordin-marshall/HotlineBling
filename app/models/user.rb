15f2290d193c2dd30ece47d68773dbdf3b73d80e
mixedbreedswag <jmarshall03@bellarmine.edu>
2015-11-27 16:03:04 -0500

    Updated front end for the new authentication system, added simple_form, updated several forms to bootstap, added login/logout/register/profile links to nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+    
+    has_many :statuses
+    
+    def full_name 
+        first_name.to_s + " " + last_name.to_s 
+    end
 end
